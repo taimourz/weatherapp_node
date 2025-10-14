1525547c8f3ac1b82bacab877948b6647a2dad96
taimourz <taimour.rkt@gmail.com>
2025-10-14 17:07:03 +0500

    use handlebars template engine

--- src/app.js
+++ src/app.js
@@ -4,17 +4,16 @@ import { fileURLToPath } from 'url'
 
 const app = express()
 
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
-app.get('', (req, res) => {
-    res.send(" <h1>Hello World</h1>")
-})
-
 const static_dir_path = path.join(__dirname, '../public')
+const views_dir_path = path.join(__dirname, '../templates')
 
+
+app.set('view engine', 'hbs')
+app.set('views', views_dir_path)
 app.use(express.static(static_dir_path))
+
 
 app.get('/weather', (req, res) => {
     res.send([
@@ -25,6 +24,31 @@ app.get('/weather', (req, res) => {
 
     ])
 })
+
+app.get('/' , (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "taimour"
+    })
+})
+
+
+app.get('/about' , (req, res) => {
+    res.render('about', {
+        title: "About me",
+        name: "created by taimour afzal"
+    })
+})
+
+
+
+app.get('/help' , (req, res) => {
+    res.render('help', {
+        msg: "You can visit this page if you need any help",
+    })
+})
+
+
 
 app.listen(3000, () => {
     console.log("Server is up and running")
